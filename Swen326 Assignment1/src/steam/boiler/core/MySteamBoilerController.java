aa2e1a53c227b664484f6dd59002d09acc313556
hoongkevi <hoongkevi@myvuw.ac.nz>
2019-04-07 00:00:24 +1300

    implemented intialization and normal modes to pass their respective tests

--- Swen326 Assignment1/src/steam/boiler/core/MySteamBoilerController.java
+++ Swen326 Assignment1/src/steam/boiler/core/MySteamBoilerController.java
@@ -8,6 +8,7 @@ import steam.boiler.util.Mailbox;
 import steam.boiler.util.SteamBoilerCharacteristics;
 import steam.boiler.util.Mailbox.Message;
 import steam.boiler.util.Mailbox.MessageKind;
+import steam.boiler.util.Mailbox.Mode;
 
 public class MySteamBoilerController implements SteamBoilerController {
 
@@ -64,23 +65,110 @@ public class MySteamBoilerController implements SteamBoilerController {
 	 */
 	@Override
 	public void clock(Mailbox incoming, Mailbox outgoing) {
+
+
 		// Extract expected messages
 		Message levelMessage = extractOnlyMatch(MessageKind.LEVEL_v, incoming);
 		Message steamMessage = extractOnlyMatch(MessageKind.STEAM_v, incoming);
 		Message[] pumpStateMessages = extractAllMatches(MessageKind.PUMP_STATE_n_b, incoming);
 		Message[] pumpControlStateMessages = extractAllMatches(MessageKind.PUMP_CONTROL_STATE_n_b, incoming);
 		//
+		double L = levelMessage.getDoubleParameter();
+		double S = steamMessage.getDoubleParameter();
+		int pumps = 1;
+		double c = getCapacity(pumpStateMessages);
+		double w = configuration.getMaximualSteamRate();
+
 		if (transmissionFailure(levelMessage, steamMessage, pumpStateMessages, pumpControlStateMessages)) {
 			// Level and steam messages required, so emergency stop.
 			this.mode = State.EMERGENCY_STOP;
 		}
 		//
-		
+		//Initialization.
+		if(extractOnlyMatch(MessageKind.PHYSICAL_UNITS_READY, incoming) != null) {
+			this.mode = State.NORMAL;
+			outgoing.send(new Message(MessageKind.MODE_m, Mode.NORMAL));
+			}
+
+		else if(this.mode == State.WAITING) {
+			outgoing.send(new Message(MessageKind.MODE_m,Mode.INITIALISATION));
+			if(extractOnlyMatch(MessageKind.STEAM_BOILER_WAITING, incoming) != null) {
+				if(steamMessage.getDoubleParameter() != 0) {
+					this.mode = State.EMERGENCY_STOP;
+				}
+				else if(steamMessage.getDoubleParameter() == 0) {
+					if(levelMessage.getDoubleParameter() > configuration.getMaximalNormalLevel()) {
+						outgoing.send(new Message(MessageKind.VALVE));
+					}else if(levelMessage.getDoubleParameter() < average(configuration.getMinimalNormalLevel(), configuration.getMaximalNormalLevel())) {
+						outgoing.send(new Message(MessageKind.OPEN_PUMP_n,0));
+						if(configuration.getMinimalNormalLevel() > predictNextLmax(pumps,L,c,w,S)) {
+						outgoing.send(new Message(MessageKind.OPEN_PUMP_n,1));
+						}else {
+							outgoing.send(new Message(MessageKind.CLOSE_PUMP_n,1));
+						}
+					}
+				}
+
+				if(levelMessage.getDoubleParameter() >= configuration.getMinimalNormalLevel() && levelMessage.getDoubleParameter() <= configuration.getMaximalNormalLevel()) {
+		/*						outgoing.send(new Message(MessageKind.CLOSE_PUMP_n,1));*/
+					outgoing.send(new Message(MessageKind.PROGRAM_READY));
+
+				}
+			}
+		}
+
+		//Normal Mode
+		else if(this.mode == State.NORMAL) {
+
+
+			if(configuration.getMinimalNormalLevel() > predictNextLmin(pumps, L, c, w, S)) {
+				pumps = pumps + 2;
+				openPumps(pumps,outgoing);
+			}else if(configuration.getMaximalNormalLevel() >  predictNextLmin(pumps, L, c, w, S)) {
+				outgoing.send(new Message(MessageKind.CLOSE_PUMP_n, 1));
+			}
+		}
+
+
+
 
 		// FIXME: this is where the main implementation stems from
 
 		// NOTE: this is an example message send to illustrate the syntax
-		outgoing.send(new Message(MessageKind.MODE_m, Mailbox.Mode.INITIALISATION));
+	}
+
+	public void openPumps(int pumps, Mailbox outgoing) {
+		int open = 0;
+		for(int i = open; i < pumps; i= i+1) {
+			outgoing.send(new Message(MessageKind.OPEN_PUMP_n,i));
+		}
+	}
+
+
+
+	private double predictNextLmax(int pumps, double L, double c, double w, double s) {
+			double Lmax = L + (5 * c * pumps) - (5 * s);
+			return Lmax;
+
+	}
+
+	private double predictNextLmin(int pumps, double L, double c, double w, double s) {
+		double Lmin = L + (5 * c * pumps) - (5 * w);
+		return Lmin;
+
+}
+	private double getCapacity(Message[] pumps) {
+		double capacity = 0;
+		for(int i = (int)capacity; i < pumps.length; i= i+1) {
+			if(pumps[i].getBooleanParameter() == true) {
+				capacity = configuration.getPumpCapacity(i);
+			}
+		}
+		return capacity;
+	}
+
+	private double average(double a, double b) {
+		return (a+b) / 2;
 	}
 
 	/**
